1c7920f80bef68560b25e5cc942ddd854f4041d4
kugimiya <bp.001@yandex.ru>
2021-10-10 19:10:41 +0400

    Refactor 'clean' command

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const kbot = new KBot({
 const manager = new KBotManager(kbot);
 
 kbot.bot.on('spawn', () => {
-  kbot.bot.chat('/login qwertyuiop');
   kbot.bot.waitForChunksToLoad();
 });
 
@@ -20,83 +19,49 @@ kbot.addCommand('kbot.come', async (self, username, message) => {
   await self.moveTo(x, y, z);
 });
 
-kbot.addCommand('kbot.borderRegion', async (self) => {
-  let position = self.bot.entity.position;
-  let width = 4;
-  
-  for (let i = 1; i <= width; i++) {
-    await manager.pushTasks(manager.borderTasksGen(i, position));
-  }
-});
-
-kbot.addCommand('kbot.clearRegion', async (self) => {
-  const position = self.bot.entity.position;
-  await manager.pushTasks(manager.clearTasksGen(16, 32, position));
-});
-
 kbot.addCommand('kbot.clear', async (self) => {
-  const voidSize = 16;
-  const voidHeight = 64;
+  const height = 72;
+  const width = 17 * 8;
+  const widthClear = width - 2;
+  let startPosition = self.bot.entity.position;
 
-  const runner = async (position) => {
-    const retFunc = async (__pos) => {
-      await self.addTask(async (_self) => {
-        await _self.sleep(500);
-        await _self.moveTo(__pos.x, __pos.y, __pos.z);
-        await _self.sleep(500);
-      });
-    }
-
-    // First level plain
-    for (let i = 1; i <= voidSize; i++) {
-      await manager.pushTasks(manager.borderTasksGen(i, position));
-    }
-
-    await retFunc(position);
-
-    // First level border
-    await manager.pushTasks(
-      manager.borderTasksGen(
-        voidSize + 2, 
-        position.offset(-1, 0, -1),
-        { stayOnBlocks: true },
-      ),
+  for (let i = 0; i < height; i++) {
+    // First level (at i > 0 this will be skipped by spiral-process algorithm)
+    await manager.processSpiralPath(
+      startPosition, 
+      width, 
+      async (refBlock) => refBlock.displayName !== 'Air', 
+      async (_self, refBlock) => {
+        await _self.bot.placeBlock(refBlock, new _self.Vec3(0, 1, 0), () => {});
+      },
+      () => {},
     );
-
-    await retFunc(position);
-
-    // And the second one :^)
-    for (let i = 1; i <= voidSize; i++) {
-      await manager.pushTasks(manager.borderTasksGen(i, position, { heightOffset: -1 }));
-    }
-
-    await retFunc(position);
-
-    // Second level border
-    await manager.pushTasks(
-      manager.borderTasksGen(
-        voidSize + 2, 
-        position.offset(-1, 0, -1),
-        {
-          heightOffset: -1,
-          stayOnBlocks: true
-        },
-      ),
+    
+    // Second level (should protect bot from fluids and voids in clear step)
+    await manager.processSpiralPath(
+      startPosition.offset(0, -1, 0), 
+      width,
+      async (refBlock) => refBlock.displayName !== 'Air', 
+      async (_self, refBlock) => {
+        await _self.bot.placeBlock(refBlock, new _self.Vec3(0, 1, 0), () => {});
+      },
+      () => {},
     );
-
-    await retFunc(position);
-
-    await manager.pushTasks(manager.clearTasksGen(voidSize, 1, position.offset(0, 0, 0)));
-
-    await retFunc(position);
-  }
-
-  let _position = self.bot.entity.position;
-  for (let level = 0; level <= voidHeight; level++) {
-    if (level > 0) {
-      _position = _position.offset(0, -1, 0);
-    }
-
-    await runner(_position);
+    
+    // Clear step
+    await manager.processSpiralPath(
+      startPosition, 
+      widthClear,
+      async (refBlock) => refBlock.displayName === 'Air', 
+      async (_self, refBlock) => {
+        await _self.bot.dig(refBlock, true);
+      },
+      async (_self, refBlock) => {
+        await _self.bot.dig(refBlock, true);
+      },
+    );
+    
+    // Delete 'center-block'
+    startPosition = startPosition.offset(0, -1, 0);
   }
 });
